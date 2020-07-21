ef3058e285da521c3384917de9a629b71016c838
Artem Kuznetzov <59560231+ArtemKuznetsovv@users.noreply.github.com>
2020-07-22 01:46:01 +0300

    Delete startup.bat

--- startup.bat
+++
@@ -1,7 +0,0 @@
-
-
-cd C:PATH TO APPLICATION.py
-SET FLASK_APP=application.py
-SET FLASK_DEBUG=1
-SET DATABASE_URL=postgres://vbxbxrrrqgiknj:e41de8ac4d66f250837a837b29b036a36208081cc6cc9e0fbb40478057718927@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d7hcg3pj1n4qvu
-cmd /k
